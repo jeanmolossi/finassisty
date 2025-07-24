8a90c68feb228337daaee26e9f5bd65bb8d38d06
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-23 23:43:59 -0300

    chore: lazy load pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Dashboard from './Dashboard'
-import InTouch from './InTouch'
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const Home = React.lazy(() => import("./Home"));
+const About = React.lazy(() => import("./About"));
+const Dashboard = React.lazy(() => import("./Dashboard"));
+const InTouch = React.lazy(() => import("./InTouch"));
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <nav className="p-4 flex gap-4 bg-gray-100">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/in-touch">In Touch</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/in-touch" element={<InTouch />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <nav className="p-4 flex gap-4 bg-gray-100">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/in-touch">In Touch</Link>
+            </nav>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Suspense fallback="Loading...">
+                            <Home />
+                        </Suspense>
+                    }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/in-touch" element={<InTouch />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
