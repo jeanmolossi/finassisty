b605ed70fd69ec12596251662e62e94a0b8ced0d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-26 11:08:36 -0300

    refactor: applied migration guide

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    outDir: 'dist'
-  }
-})
+    plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        outDir: "dist",
+    },
+});
