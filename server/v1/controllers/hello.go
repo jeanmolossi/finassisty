130db7b770df49a10e0b7091021fc957ec53b18d
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-28 01:05:43 -0300

    chore: simulate error at hello

--- server/v1/controllers/hello.go
+++ server/v1/controllers/hello.go
@@ -1,13 +1,20 @@
 package controllers
 
 import (
-	"log/slog"
+	"math/rand/v2"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Hello(c echo.Context) error {
-	slog.Info("http request")
-	return c.JSON(http.StatusOK, map[string]string{"message": "hello"})
+	now := time.Now().Format(time.RFC3339)
+
+	err := rand.IntN(100) < 10 // simulate 10% error
+	if err {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "failed from " + now})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "hello from " + now})
 }
